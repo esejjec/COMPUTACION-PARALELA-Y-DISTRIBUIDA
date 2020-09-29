d13452a0c860f7984543218b718e8afaa16435e0
Erika Sejje <esejjec@unsa.edu.pe>
2020-09-28 20:56:04 -0500

    Actualización

--- TEORIA/PingPong.c
+++ TEORIA/PingPong.c
@@ -3,45 +3,52 @@
 
 #define PROC_A 0
 #define PROC_B 1
-#define TAG 10 //LONG_MSJ
+#define TAG 0 //LONG_MSJ
 #define ITERACIONES 10
-#define CON_ball  0
+//#define CON_ball  0
 
 void proceso_A(); //Ping
 void proceso_B(); //Pong
 
 int main(void){
-	int n, myid, my_rank;
+	MPI_Init(NULL, NULL);
+	int my_rank, my_size;
+	MPI_Comm_size(MPI_COMM_WORLD, &my_size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+ 
 
-	MPI_Init(NULL,NULL);
-	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);	
-	MPI_Comm_size(MPI_COMM_WORLD, &myid);
+	int sender, recver;
+	
+	int CON_ball = 0;
+	//Implementacion Basica
+	while(CON_ball < ITERACIONES)
+	{
+		sender = CON_ball % 2;
+        recver = 1 - sender;
+        
+        if ( my_rank == sender ){
+       		MPI_Send(&CON_ball, 1, MPI_INT, recver, TAG , MPI_COMM_WORLD);
+          	printf("%d Send sendbuffer %d from %d\n",
+             my_rank, CON_ball, sender);
+        }
+        else {
+          MPI_Recv(&CON_ball, 1, MPI_INT, sender, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+          printf("%d received sendbuffer %d from %d\n",
+             my_rank, CON_ball, sender);
+        }
+
+		
+		MPI_Barrier(MPI_COMM_WORLD);
+		//printf("Process %d iter %d ball value is : %d\n",my_rank,i,CON_ball);
+
+	}
+
 
 	/*
-	
-	//Implementacion Basica
-	while(CON_ball != ITERACIONES)
-	{
-		if (my_rank == PROC_A){
-			MPI_Send( &CON_ball , 1 , MPI_INTEGER , PROC_B , TAG , MPI_COMM_WORLD ); // 0 send CON_ball to 1, and wait for transfer to be finished
-			MPI_Recv( &CON_ball , 1 , MPI_INTEGER , PROC_B , TAG , MPI_COMM_WORLD, MPI_STATUS_IGNORE); // 0 receive CON_ball from 1, and wait for transfer to be finished
-			CON_ball = CON_ball + 1;
-		}
-		if (my_rank == PROC_B){
-			MPI_Recv( &CON_ball , 1 , MPI_INTEGER , PROC_A , TAG , MPI_COMM_WORLD, MPI_STATUS_IGNORE );
-			CON_ball = CON_ball + 1;
-			MPI_Send( &CON_ball , 1 , MPI_INTEGER , PROC_A , TAG , MPI_COMM_WORLD );
-
-		}
-
-		printf("Process %d iter %d ball value is : %d\n",my_rank,i,CON_ball);
-
-	}*/
-
 	//Implementacion 2
 	if (my_rank == PROC_A) proceso_A();
-	else if (my_rank == PROC_B) proceso_B();
-
+	else if (my_rank == PROC_B) proceso_B();*/
+	
 	MPI_Finalize();
 
 	return 0;
@@ -63,7 +70,7 @@ void proceso_A(){
 		}
 	}
 
-	CON_ball = CON_ball + 1;
+	//CON_ball = CON_ball + 1;
 
 	time = MPI_Wtime() - time;
 	printf("%f\t \n",time);
@@ -84,7 +91,7 @@ void proceso_B(){
 			MPI_Ssend(Mensaje, i, MPI_INTEGER, PROC_A, 10,MPI_COMM_WORLD);
 		}
 	}
-	CON_ball = CON_ball + 1;
+	//CON_ball = CON_ball + 1;
 	time = MPI_Wtime() - time;
 	printf("%f\t \n",time);
 }
